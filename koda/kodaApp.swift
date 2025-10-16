efb914afdee557324d40bb4b0affdb6b4239362f
oeuvars <sudo@anurag.gg>
2025-10-16 11:33:53 +0530

    Initial Commit

---
+++ koda/kodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kodaApp.swift
+//  koda
+//
+//  Created by Anurag Das on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct kodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
